4f82504bd923d9fdebb802782c7994e90a073a05
Ricky Macharm <Ricky.Macharm@SisengAI.com>
2025-01-30 13:53:58 +0100

    question and answer

---
+++ 02_building_capable_assistants/question_and_answer/agent.py
@@ -0,0 +1,36 @@
+"""Defining an agent."""
+
+from typing import Literal
+
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain.chains import Chain
+from langchain_experimental.plan_and_execute import (
+    PlanAndExecute,
+    load_agent_executor,
+    load_chat_planner,
+)
+from langchain_openai import ChatOpenAI
+
+from question_and_answer.tool_loader import load_tools
+from config import set_environment
+
+set_environment()
+
+ReasoningStrategies = Literal["zero-shot-react", "plan-and-solve"]
+
+
+def load_agent(
+    tool_names: list[str], strategy: ReasoningStrategies = "zero-shot-react"
+) -> Chain:
+    llm = ChatOpenAI(temperature=0, streaming=True)
+    tools = load_tools(tool_names=tool_names, llm=llm)
+    if strategy == "plan-and-solve":
+        planner = load_chat_planner(llm)
+        executor = load_agent_executor(llm, tools, verbose=True)
+        return PlanAndExecute(planner=planner, executor=executor, verbose=True)
+
+    prompt = hub.pull("hwchase17/react")
+    return AgentExecutor(
+        agent=create_react_agent(llm=llm, tools=tools, prompt=prompt), tools=tools
+    )
---
+++ 02_building_capable_assistants/question_and_answer/app.py
@@ -0,0 +1,63 @@
+"""Question answering app in Streamlit.
+
+
+Run locally as follows:
+> PYTHONPATH=. streamlit run question_and_answer/app.py
+
+Alternatively, you can deploy this on the Streamlit Community Cloud
+or on Hugging Face Spaces. For Streamlit Community Cloud do this:
+1. Create a GitHub repo
+2. Go to Streamlit Community Cloud, click on "New app" and select the new repo
+3. Click "Deploy!"
+"""
+
+import streamlit as st
+from langchain_community.callbacks.streamlit import (
+    StreamlitCallbackHandler,
+)
+
+from question_and_answer.agent import load_agent
+from question_and_answer.utils import MEMORY
+
+st.set_page_config(page_title="LangChain Question Answering", page_icon=":robot:")
+st.header("Ask a research question!")
+
+strategy = st.radio(
+    "Reasoning strategy",
+    (
+        "plan-and-solve",
+        "zero-shot-react",
+    ),
+)
+
+tool_names = st.multiselect(
+    "Which tools do you want to use?",
+    [
+        "critical_search",
+        "llm-math",
+        "python_repl",
+        "wikipedia",
+        "arxiv",
+        "google-search",
+        "wolfram-alpha",
+        "ddg-search",
+    ],
+    ["ddg-search", "wolfram-alpha", "wikipedia"],
+)
+if st.sidebar.button("Clear message history"):
+    MEMORY.chat_memory.clear()
+
+avatars = {"human": "user", "ai": "assistant"}
+for msg in MEMORY.chat_memory.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+
+assert strategy is not None
+agent_chain = load_agent(tool_names=tool_names, strategy=strategy)
+
+
+if prompt := st.chat_input(placeholder="Ask me anything!"):
+    st.chat_message("user").write(prompt)
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent_chain.invoke({"input": prompt}, {"callbacks": [st_callback]})
+        st.write(response["output"])
